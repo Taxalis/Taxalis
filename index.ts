0c377d7beda1d2afabf4260a1dd0f8a669212440
Taxalis <info@taxalis-consulting.de>
2026-07-09 13:33:28 +0200

    Delete index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-import siteSettings from "./siteSettings";
-import testimonial from "./testimonial";
-import faqItem from "./faqItem";
-
-export const schemaTypes = [siteSettings, testimonial, faqItem];
